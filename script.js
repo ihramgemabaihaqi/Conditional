84d5d68937d67325e0554701fb447c11d86ea748
ihramgemabaihaqi <ihramgemaa@icloud.com>
2024-11-07 13:37:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById(OrderButton).onclick= function() {
+    let foodPrice = parseInt(document.getElementById) ("food").value;
+    let drinkPrice = parseInt(document.getElementById)("drink").value;
+
+    let totalprice = foodPrice + drinkPrice;
+
+   let discount = 0;
+
+   if (totalPrice > 90) {
+    alert('Ok');
+   }
+}
